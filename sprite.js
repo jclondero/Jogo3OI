49d36279d3ef49e13667dc78c70ae19aa8f99c29
Tiago <tiago.r.traca@gmail.com>
2018-04-27 19:58:32 -0300

    joguinho 3

---
+++ sprite.js
@@ -0,0 +1,17 @@
+function Sprite(x,y,largura,altura) {
+
+    this.x = x;
+    this.y = y;
+    this.altura = altura;
+    this.largura = largura;
+
+    this.desenha = function(xCanvas, yCanvas){
+        ctx.drawImage(img, this.x, this.y, this.largura, this.altura, xCanvas, yCanvas, this.largura, this.altura)
+    }
+
+    
+    
+}
+
+var bg = new Sprite(0, 0, 1200, 600),
+vidraria = new Sprite(0,0,0,0);
